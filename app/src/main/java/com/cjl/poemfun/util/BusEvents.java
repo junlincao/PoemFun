a7c83b1afaeb921a122eb3bf5698a9fba5e75c1e
CJL <cjltgs@163.com>
2015-04-15 21:11:52 +0800

    busEvents

---
+++ app/src/main/java/com/cjl/poemfun/util/BusEvents.java
@@ -0,0 +1,26 @@
+package com.cjl.poemfun.util;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * otto 事件
+ *
+ * @author CJL
+ * @since 2015-04-15
+ */
+public class BusEvents {
+    /** 首页导航栏内容跳转事件 **/
+    public static class NavDrawerItemClickEvent {
+        private Fragment toFragment;
+
+        public Fragment getToFragment() {
+            return toFragment;
+        }
+
+        public void setToFragment(Fragment toFragment) {
+            this.toFragment = toFragment;
+        }
+    }
+
+
+}
