574671e1cc858512d244646ec1d40f80dbbd2f97
junlincao <junlincaoit@gmail.com>
2015-04-24 00:05:27 +0800

    BookLikePager --test

--- app/src/main/java/com/cjl/poetryfan/ui/widget/BookLikePager.java
+++ app/src/main/java/com/cjl/poetryfan/ui/widget/BookLikePager.java
@@ -12,8 +12,8 @@ import android.view.View;
 
 /**
  * ViewPager 书页效果
- * <p/>
- * 不要设置pager 的 Left padding, 其它的padding可以随意设置
+ * <p>
+ * 不要设置pager 的 Left and right padding
  *
  * @author CJL
  * @since 2015-04-23
@@ -22,7 +22,7 @@ public class BookLikePager extends ViewPager {
     public interface IBookLikePagerAdapter {
         /**
          * 根据ItemView 对象获取其对于在adapter中的位置
-         * <p/>
+         * <p>
          * 建议使用tag存储其位置
          *
          * @param itemView adapter 的一个itemVIew
@@ -38,12 +38,13 @@ public class BookLikePager extends ViewPager {
 
 
     private int mMaxEdgeWidth;
-    private int mMaxEdgeCount = 2;
+    private int mMaxEdgeCount = 3;
     private float mScrollOffset;
     private Drawable mEdgeDrawable;
 
     private int mFirstOffset;
     private int mNowEdgeCount;
+    private int mItemEdgeWidth;
 
 
     public BookLikePager(Context context) {
@@ -61,18 +62,28 @@ public class BookLikePager extends ViewPager {
 
         mMaxEdgeWidth = (int) (density * 15 + .5);
 
-        setPageTransformer(false, null);
+        setPageTransformer(false, null); // set the default PageTransformer
+        setClipToPadding(false);
+        resetPadding();
     }
 
     public void setMaxPageEdgeCount(int count) {
-        if (count <= 1) {
-            throw new IllegalArgumentException("count can't be less than 1");
+        if (count <= 2) {
+            throw new IllegalArgumentException("count can't be less than 2");
         }
         this.mMaxEdgeCount = count;
+        resetPadding();
     }
 
     public void setMaxPageEdgeWidth(int width) {
         mMaxEdgeWidth = width;
+        resetPadding();
+    }
+
+    private void resetPadding() {
+        int padding = (int) (mMaxEdgeWidth * (mMaxEdgeCount + 1) / mMaxEdgeCount / 2f);
+        setPadding(padding, getPaddingTop(), padding, getPaddingBottom());
+        mItemEdgeWidth = mMaxEdgeWidth / mMaxEdgeCount;
     }
 
     @Override
@@ -114,37 +125,110 @@ public class BookLikePager extends ViewPager {
 
     /**
      * @param view   PagerAdapter 的一个itemView
-     * @param offset 页ID减少时应大于0
+     * @param offset 从屏幕左边进-1->0, 出0->-1
+     *               屏幕右边进 1->0, 出0->1
      * @param idx    view 在adapter中的position
      */
     private void onTransformPage(View view, float offset, int idx) {
-        if(idx == getCurrentItem()){
-            if(offset < 0){
-                setStickPage(view, offset);
-            } else {
-                setMovePage(view, offset);
-            }
-            updateState();
-        } else if(idx == getCurrentItem() - 1){
-            if(offset < 0){
-                setStickPage(view, offset);
-            }
+        view.setTranslationX(calcTranslationX(idx, offset));
+    }
+
+    /**
+     * @param idx    page 在adapter 中的idx
+     * @param offset 从屏幕左边进-1->0, 出0->-1
+     *               屏幕右边进 1->0, 出0->1
+     * @return ViewPager 偏移位置
+     */
+    private int calcTranslationX(int idx, float offset) {
+        final int cIdx = getCurrentItem();
+
+        int from, to;
+        if(offset < 0){
+            from = getDefaultX(idx - 1, cIdx);
+            to = getDefaultX(idx, cIdx);
+
+        } else if(offset > 0){
+            from = getDefaultX(idx, cIdx);
+            to = getDefaultX(idx + 1, cIdx);
         } else {
-            if(offset > 0){
-                setMovePage(view, offset);
-            }
+            from = to = 0;
         }
 
-        Log.d("---", idx + "->" + offset);
+        int delta = (int) ((to - from) * offset);
+        int defTranslationX = -(idx - cIdx) * getWidth();
+
+
+        Log.d("---", idx + "->offset=" + offset + ";\tfrom=" + from + ";\tto=" + to + ";\tx=" + defTranslationX);
+
+        return delta + defTranslationX;
+
+
+
+
+
+
+
+
+//        if (idx > cIdx) {
+//            if (offset >= 0) {
+//                from = getDefaultX(idx - 1, cIdx);
+//                to = getDefaultX(idx, cIdx);
+//
+//            } else {
+//                from = getDefaultX(idx, cIdx);
+//                to = getDefaultX(idx - 1, cIdx);
+//            }
+//
+//        } else if (idx == cIdx) {
+//            if (offset >= 0) {
+//                from = getDefaultX(idx, cIdx);
+//                to = getDefaultX(idx, cIdx - 1);
+//            } else {
+//                from = getDefaultX(idx, cIdx - 1);
+//                to = getDefaultX(idx, cIdx);
+//            }
+//        } else {
+//            if (offset >= 0) {
+//                from = getDefaultX(idx, cIdx);
+//                to = getDefaultX(idx + 1, cIdx);
+//
+//            } else {
+//                from = getDefaultX(idx + 1, cIdx);
+//                to = getDefaultX(idx, cIdx);
+//            }
+//
+//        }
+//
+//        int delta = (int) ((to - from) * offset);
+//        int defTranslationX = (idx - cIdx) * getWidth();
+//
+//        return delta + defTranslationX;
     }
 
-    private void setMovePage(View view, float offset){
-//        view.setTranslationX(getWidth() * offset);
-    }
-
-    private void setStickPage(View view, float offset){
-        view.setTranslationX(-getWidth() * offset);
-
+    /**
+     * 获取指定页应该所处的x位置
+     *
+     * @param idx 页id
+     * @param ci  当前默认页ID
+     * @return 指定页x位置
+     */
+    private int getDefaultX(int idx, int ci) {
+        if (idx > ci) { // 大于当前显示页，应该是在屏幕右边看不见状态
+            return getWidth();
+        } else if (idx == ci) {
+            if (idx < mMaxEdgeCount) { //小于最大页数，所有页一起居中显示，计算偏差
+                int delta = getPaddingLeft() - (idx + 1) * mItemEdgeWidth / 2;
+                return delta + (idx + 1) * mItemEdgeWidth;
+            } else { // 在最上面最右边
+                return mMaxEdgeCount * mItemEdgeWidth;
+            }
+        } else { // 小于当前显示页
+            if (ci - idx > mMaxEdgeCount - 1) { // 相隔超过最大页数，在屏幕左边
+                return -getWidth();
+            } else {
+                return (idx + mMaxEdgeCount - ci) * mItemEdgeWidth;
+            }
+        }
     }
 
     private void updateState() {
@@ -155,18 +239,7 @@ public class BookLikePager extends ViewPager {
         int currentItem = getCurrentItem();
 
 
-        if (currentItem <= mMaxEdgeCount) {
-            mFirstOffset = 0;
-            int leftPadding = (int) (currentItem * 1f * mMaxEdgeWidth / mMaxEdgeCount);
-            setPaddingLeft(leftPadding);
-            return;
-        }
-
         mNowEdgeCount = Math.min(currentItem, mMaxEdgeCount) + 1;
-    }
-
-    private void setPaddingLeft(int leftPadding) {
-        setPadding(leftPadding, getPaddingTop(), getPaddingRight(), getPaddingBottom());
     }
 
     @Override
